d5c5db0ac12714b7273f7714985336ac031417eb
Vito Medlej <vito.medlej@gmail.com>
2024-02-08 11:11:55 +0200

    order fix

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -61,7 +61,7 @@ export default function Checkout() {
     const handleNext = () => {
       if (info?.checkbox2 && info?.checkbox && info && info.email && info.firstName && info.lastName && info.address1 && info.phone) {
         localStorage.setItem('25VNSFKJNJKNX14hhJ52cknv',JSON.stringify({...info, name : `${info.firstName} ${info.lastName}`}))
-        saveState('2VI1H2OI-FJ04BJZ2X',info)
+        saveState('2G184N24-JZ094512JIF12412',info)
         setActiveStep(activeStep + 1);
       
       } 
