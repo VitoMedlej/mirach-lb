28bda2b3770e4c56806ee2ce85f21c65243d5109
Vito Medlej <vito.medlej@gmail.com>
2024-06-21 16:20:30 +0300

    try catch

--- src/Utils/Getcategories.ts
+++ src/Utils/Getcategories.ts
@@ -1,7 +1,14 @@
 import { cache } from 'react';
 
 export const Getcategories = cache(async () => {
+try {
+
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-categories`,{next:{revalidate:5}});
   const data = await res.json();
   return data;
+}
+catch(err) {
+  console.log('err: ', err);
+
+}
 });
--- src/Utils/Getproducts.ts
+++ src/Utils/Getproducts.ts
@@ -1,7 +1,15 @@
 import { cache } from 'react';
 
 export const Getproducts = cache(async () => {
+  try {
+
+  
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data?tkn=${process.env.token}`,{next:{revalidate:5}});
   const data = await res.json();
   return data;
+}
+catch(err) {
+  console.log('err: ', err);
+
+}
 });
